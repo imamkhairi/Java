8f6ce534ff46a8094244aba91a1174e4226d0d55
imamkhairi <imamkhairilubis@gmail.com>
2022-04-19 00:07:03 +0900

    update report2

--- 2/KozaApp3.java
+++ 2/KozaApp3.java
@@ -2,10 +2,10 @@ class KozaApp3 {
     public static void main(String[] args) {
         Koza my_koza; 
         my_koza = new Koza();
-
+        
         my_koza.gankin = Integer.parseInt(args[0]);
         int i;
-        for(i = 1; i <= 36; i++){
+        for(i = 1; i <= Integer.parseInt(args[1]); i++){
             System.out.println("利息は" + my_koza.risoku(i));
         }
 
