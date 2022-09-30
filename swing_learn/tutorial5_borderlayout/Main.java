97484921fa97228f7b03354188afab3d8a2a9b31
imamkhairi <imamkhairilubis@gmail.com>
2022-09-30 09:11:03 +0900

    learn_swing

---
+++ swing_learn/tutorial5_borderlayout/Main.java
@@ -0,0 +1,14 @@
+package swing_learn.tutorial5_borderlayout;
+
+import javax.swing.JFrame;
+
+public class Main {
+    
+    // Layout Manager = Defines the natural layout for components within a container
+
+    // 3 common managers
+
+    // BorderLayout = a BorderLayout places components in five areas: NORTH, SOUTH, WEST, EAST, CENTER  
+    //                All extra spaces is placed in the center area
+
+}
